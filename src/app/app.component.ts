89d3a712cc9dacd4dcde1f5fdf8767c9cf41ee12
Lezcano Tech  <153436452+Oliver-Adamtey@users.noreply.github.com>
2024-01-26 14:50:23 +0000

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,5 +10,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-course-amalitech';
+  title = 'Angular Core Deep Dive';
 }
