2b502723563162386041b34c162dd63efbe0509c
Lezcano Tech  <153436452+Oliver-Adamtey@users.noreply.github.com>
2024-01-31 11:12:52 +0000

    updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,4 +11,8 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'Angular Core Deep Dive';
+
+  onKeyUp (newTitle:string) {
+    this.title = newTitle;
+  }
 }
